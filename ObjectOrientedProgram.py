a69934f66ba681a9a93e3c0c64d3cf0699eacacb
Alex <123321081+Goofball554@users.noreply.github.com>
2023-03-14 17:34:54 -0400

    Updated code to include Classes

--- ObjectOrientedProgram.py
+++ ObjectOrientedProgram.py
@@ -1,67 +1,106 @@
 import random
-codeCheck = False
-saleCheck = False
-costCheck = False
-stockCheck = False
-estimateCheck = False
 
-totalSold = 0
+class Application:
+    code = 0
+    name = 0
+    cost = 0
+    sale = 0
+    stock = 0
+    estimate = 0
 
+    def getCode(code):
+        # Asks the user to input the Product Code and checks whether the input is valid.
+        codeCheck = False
+        while codeCheck == False:
+            code = input("Please enter the Product Code: ")
+            if float(code) == int(code) and int(code) >= 100 and int(code) <= 1000:
+                codeCheck = True
+        return code
 
+    def getName(name):
+        #Asks the user to input the Product Name.
+        name = input("Please enter the Product Name: ")
 
-# Asks the user to input the Product Code and checks whether the input is valid.
-while codeCheck == False:
-    code = input("Please enter the Product Code: ")
+        return name
 
-    if float(code) == int(code) and int(code) >= 100 and int(code) <= 1000:
-        codeCheck = True
+    def getSale(sale):
+        #Asks the user to input the Product Sale Price and checks whether the input is valid.
+        saleCheck = False
+        while saleCheck == False:
+            sale = input("Please enter the Product Sale Price: ")
+            if float(sale) > 0:
+                saleCheck = True
 
-#Asks the user to input the Product Name.
-name = input("Please enter the Product Name: ")
+        return sale
 
-#Asks the user to input the Product Sale Price and checks whether the input is valid.
-while saleCheck == False:
-    sale = input("Please enter the Product Sale Price: ")
+    def getCost(cost):
+        #Asks the user to input the Product Manufacture Cost and checks whether the input is valid.
+        costCheck = False
+        while costCheck == False:
+            cost = input("Please enter the Product Manufacture Cost: ")
+            if float(cost) > 0:
+                costCheck = True
 
-    if float(sale) > 0:
-        saleCheck = True
+        return cost
 
-#Asks the user to input the Product Manufacture Cost and checks whether the input is valid.
-while costCheck == False:
-    cost = input("Please enter the Product Manufacture Cost: ")
+    def getStock(stock):
+        #Asks the user to input the Stock Level and checks whether the input is valid.
+        stockCheck = False
+        while stockCheck == False:
+            stock = input("Please enter the Stock Level: ")
+            if float(stock) == int(stock) and int(stock) > 0:
+                stockCheck = True
 
-    if float(cost) > 0:
-        costCheck = True
+        return stock
 
-#Asks the user to input the Stock Level and checks whether the input is valid.
-while stockCheck == False:
-    stock = input("Please enter the Stock Level: ")
+    def getEstimate(estimate):
+        #Asks the user to input the Estimated Monthly Units Manufactured and checks whether the input is valid. 
+        estimateCheck = False
+        while estimateCheck == False:
+            estimate = input("Please enter the Estimated Monthly Units Manufactured: ")
+            if float(estimate) == int(estimate) and int(estimate) >= 0:
+                estimateCheck = True
 
-    if float(stock) == int(stock) and int(stock) > 0:
-        stockCheck = True
+        return estimate
+    
+    getCode()
+    getName()
+    getCost()
+    getSale()
+    getStock()
+    getEstimate()
 
-#Asks the user to input the Estimated Monthly Units Manufactured and checks whether the input is valid. 
-while estimateCheck == False:
-    estimate = input("Please enter the Estimated Monthly Units Manufactured: ")
+class Logic(Application):
+    def printStocks():
+        #Prints the first half of the stock statement.
+        print("******* Programming Principles Sample Stock Statenent *******")
+        print("Product Code: ", Application.code)
+        print("Product Name: ", Application.name)
+        print("Sale Price: ", Application.sale, " CAD")
+        print("Manufacture Cost: ", Application.cost, " CAD")
+        print("Monthly Production: ", Application.estimate, " units (Approx.)")
 
-    if float(estimate) == int(estimate) and int(estimate) >= 0:
-        estimateCheck = True
+        estimate = Application.estimate
+        stock = Application.stock
+        sale = Application.sale
+        cost = Application.cost
+        totalSold = 0
 
-    print("******* Programming Principles Sample Stock Statenent *******")
-    print("Product Code: ", code)
-    print("Product Name: ", name)
-    print("Sale Price: ", sale, " CAD")
-    print("Manufacture Cost: ", cost, " CAD")
-    print("Monthly Production: ", estimate, " units (Approx.)")
+        #Prints the monthly portion of the stock statement.
+        for i in range(12):
+            sold = random.randrange(int(estimate) - 10, int(estimate) + 11)
+            stock = int(stock) + int(estimate) - int(sold)
+            totalSold = totalSold + int(sold)
+            print("Month ", i + 1, ":")
+            print("*     Manufactured: ", estimate)
+            print("*     Sold: ", sold)
+            print("*     Stock:", stock)
+            
+        #Prints the net profit of the stock statement.
+        netProfit = totalSold * (float(sale) - float(cost))
+        print("Net Profit: ", netProfit, " CAD")
 
-    for i in range(12):
-        sold = random.randrange(int(estimate) - 10, int(estimate) + 11)
-        stock = int(stock) + int(estimate) - int(sold)
-        totalSold = totalSold + int(sold)
-        print("Month ", i + 1, ":")
-        print("*     Manufactured: ", estimate)
-        print("*     Sold: ", sold)
-        print("*     Stock:", stock)
+    printStocks()
 
-    netProfit = totalSold * (float(sale) - float(cost))
-    print("Net Profit: ", netProfit, " CAD")
+runProgram = Application()
+printStock = Logic()
